da0c8db10ded75017a416b81786f0a3861a87612
wayne mankertz <wdm2005@blueyonder.co.uk>
2012-08-23 04:52:31 +0100

    added missing templates

---
+++ Themes/blue/languages/english.php
@@ -0,0 +1,14 @@
+<?php
+
+global $boardurl;
+
+$txt = array();
+$txt['home'] = 'Home';
+$txt['msgs'] = 'Messages';
+$txt['myspace'] = 'MySpace';
+$txt['twitter'] = 'Tweet';
+$txt['facebook'] = 'Facebook';
+$txt['whos_on'] = 'Who&#39;s Online';
+$txt['guest_msg'] = 'Welcome, Guest. Please <a class="white" href="'.$boardurl.'/index.php?action=login">login</a> or <a class="white" href="'.$boardurl.'/index.php?action=register">register.</a>';
+$txt['load_warning'] = 'The chat is not availible due to high server load.';
+?>
---
+++ Themes/blue/languages/portuguse_brazillian.php
@@ -0,0 +1,14 @@
+<?php
+
+global $boardurl;
+
+$txt = array();
+$txt['home'] = 'Home';
+$txt['msgs'] = 'Mensagens';
+$txt['myspace'] = 'MySpace';
+$txt['twitter'] = 'Tweet';
+$txt['facebook'] = 'Facebook';
+$txt['whos_on'] = 'Quem est&aacute; Online';
+$txt['guest_msg'] = 'Bem-vindo, Visitante. Por favor <a class="white" href="'.$boardurl.'/index.php?action=login">Entrar</a> ou <a class="white" href="'.$boardurl.'/index.php?action=register">registar-se.</a>';
+$txt['load_warning'] = 'O Chat n&atilde;o esta disponível, devido à alta carregar do servidor.';
+?>
