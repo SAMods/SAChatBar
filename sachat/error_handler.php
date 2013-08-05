531d11c2575b95c2947219125fd5f87257957ce6
SAMods <wdm2005@blueyonder.co.uk>
2013-08-05 05:43:27 +0100

    rename logdb() function  to logError()

--- sachat/error_handler.php
+++ sachat/error_handler.php
@@ -6,27 +6,27 @@ function errorHandler($error_level, $error_message, $error_file, $error_line, $e
         case E_CORE_ERROR:
         case E_COMPILE_ERROR:
         case E_PARSE:
-            logdb($error_file, $error_line, $error_message, "Fatal");
+            logError($error_file, $error_line, $error_message, "Fatal");
             break;
         case E_USER_ERROR:
         case E_RECOVERABLE_ERROR:
-            logdb($error_file, $error_line, $error_message, "ERROR");
+            logError($error_file, $error_line, $error_message, "ERROR");
         break;
         case E_WARNING:
         case E_CORE_WARNING:
         case E_COMPILE_WARNING:
         case E_USER_WARNING:
-            logdb($error_file, $error_line, $error_message, "WARN");
+            logError($error_file, $error_line, $error_message, "WARN");
             break;
         case E_NOTICE:
         case E_USER_NOTICE:
-            logdb($error_file, $error_line, $error_message, "INFO");
+            logError($error_file, $error_line, $error_message, "INFO");
             break;
         case E_STRICT:
-           logdb($error_file, $error_line, $error_message, "DEBUG");
+           logError($error_file, $error_line, $error_message, "DEBUG");
             break;
         default:
-            logdb($error_file, $error_line, $error_message, "WARN");
+            logError($error_file, $error_line, $error_message, "WARN");
     }
 }
 
@@ -43,12 +43,12 @@ function shutdownHandler() //will be called when php script ends.
         case E_CORE_WARNING:
         case E_COMPILE_WARNING:
         case E_PARSE:
-        logdb($lasterror['file'], $lasterror['line'],  $lasterror['message'], "FATAL");
+        logError($lasterror['file'], $lasterror['line'],  $lasterror['message'], "FATAL");
 
     }
 }
 
-function logdb($errorfile, $errorline, $errormes, $errortype){
+function logError($errorfile, $errorline, $errormes, $errortype){
   global $smcFunc, $last_error;
 
    $error_info = array($errortype,$errorfile,$errormes,$errorline); 
